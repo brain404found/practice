3e2531d0b34bb7b3de1840055dbc90637a800da7
Jamshed <jamshedjuraev95@gmail.com>
2025-06-13 15:40:15 +0500

    http handler added

--- go/patterns/rate_limiter/go.mod
+++ go/patterns/rate_limiter/go.mod
@@ -1,3 +1,5 @@
 module rate-limiter
 
 go 1.22.4
+
+require github.com/brianvoe/gofakeit v3.18.0+incompatible
--- go/patterns/rate_limiter/main.go
+++ go/patterns/rate_limiter/main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/brianvoe/gofakeit"
+)
+
 func main() {
-	
+	http.HandleFunc("/note", GetNote)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GetNote(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	response := struct {
+		Id    int    `json:"id"`
+		Title string `json:"title"`
+	}{
+		Id:    gofakeit.Number(1, 1000),
+		Title: gofakeit.Sentence(10),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
 }
